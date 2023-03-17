9e629fcb2ef15861d8d8b18e0df9ce60c35458c6
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 13:55:28 +0700

    first commit

---
+++ src/main/kotlin/Latihan.kt
@@ -0,0 +1,12 @@
+fun main(){
+    println("Hello friend!")
+    println("Farhan")
+
+    println("pekerjaan : ")
+    val pekerjaan = readLine()
+
+    println("alamat : ")
+    val alamat = readLine()
+
+    println("Pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+}
